b0cc3e5a65e866c77e4d78e66fccd1039086e909
Kristof Dreier <kontakt@kristofdreier.de>
2013-11-09 12:00:40 +0100

    added sample local config file

---
+++ local-config.SAMPLE.php
@@ -0,0 +1,12 @@
+<?php
+/*
+This is a sample local-config.php file
+In it, you *must* include the four main database defines
+
+You may include other settings here that you only want enabled on your local development checkouts
+*/
+
+define( 'DB_NAME', 'DATABASE NAME' );
+define( 'DB_USER', 'DATABASE USER' );
+define( 'DB_PASSWORD', 'DATABASE PASSWORD' );
+define( 'DB_HOST', 'DATABASE HOST' ); // Probably 'localhost'
